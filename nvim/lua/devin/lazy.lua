085bf01ee8c6dc5d7976fa1d5115fd811836fc5a
Devin Gendron <gendron.devin@gmail.com>
2024-04-22 13:35:44 -0400

    further plugins added

--- nvim/lua/devin/lazy.lua
+++ nvim/lua/devin/lazy.lua
@@ -12,4 +12,12 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("devin.plugins")
+require("lazy").setup("devin.plugins", {
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        notify = false,
+    },
+})
